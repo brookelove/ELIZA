71ab83bb124f12aeba4ad887fccb06e40b91a2a4
Brooke Love <savienloveb@gmail.com>
2022-09-17 09:27:27 -0400

    adding inital information on greeting and user input

--- build01.py
+++ build01.py
@@ -4,3 +4,8 @@ Postconditions
 2 (Complaint): A complaint was entered by the user in response to a prompt
 AND Eliza has responded "I am sorry to hear you report <the complaint entered by the user>"
 '''
+print("\nThank you for using Eliza300, a fun therapy program.\nPlease state your emotional complaint then hit 'ENTER':\n")
+
+user_input = input()
+
+print('\nI am sorry to hear you report "' + user_input + '"\n')
