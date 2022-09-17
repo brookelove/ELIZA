068784d7141aae0c0f256a1332b6c2fff80f7af7
Brooke Love <savienloveb@gmail.com>
2022-09-17 09:16:44 -0400

    adding in post conditions

---
+++ build01.py
@@ -0,0 +1,6 @@
+'''
+Postconditions
+1 (Welcome): A welcome message is on the console
+2 (Complaint): A complaint was entered by the user in response to a prompt
+AND Eliza has responded "I am sorry to hear you report <the complaint entered by the user>"
+'''
