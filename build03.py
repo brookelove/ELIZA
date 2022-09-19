b480088e7f7231f9a7606859bc07b83ba087022f
Brooke Love <savienloveb@gmail.com>
2022-09-19 07:13:52 -0400

    creating postconditions for ELIZA

---
+++ build03.py
@@ -0,0 +1,11 @@
+'''
+Eliza300: Postconditions
+1. (Welcome): A welcome message is on the console
+2. (Complaint): A complaint was entered by the user in response to a prompt
+3. (Duration): A duration in months was entered by user in response to a prompt
+4. (Error check): EITHER the user entered an integer between 1 and 100 for duration after being given up to two chances OR the application quit after suggesting a re-run.
+5. (Action Recommended): EITHER how_long exceeds 2 months, and the phrase
+   “ … months is too much time to go without help! Let's schedule a few 
+   sessions" is on the console OR the following is on the console:
+   "Come back in a couple of months if this persists".
+'''
