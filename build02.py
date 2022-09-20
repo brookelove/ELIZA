8f500ee09532be9042c7aec4b720cfa02527d820
Brooke Love <savienloveb@gmail.com>
2022-09-20 10:46:58 -0400

    creating build 02 and build 03

--- build02.py
+++ build02.py
@@ -9,3 +9,13 @@ Eliza300: Postconditions
    OR the following is on the console:
    "Come back in a couple of months if this persists".
 '''
+print("\nThank you for using Eliza300, a fun therapy program.\nPlease state your emotional complaint then hit 'ENTER':")
+
+user_input = input()
+
+print('How many months has it been that you have experienced "' + user_input + '"')
+months = input()
+if int(months) <= 2: 
+    print ("Come back in a couple of months if this persists")
+if int(months) >=3:
+    print(months + " months is too much time to go without help! Lets schedule a few sessions.")
--- build03.py
+++ build03.py
@@ -9,3 +9,18 @@ Eliza300: Postconditions
    sessions" is on the console OR the following is on the console:
    "Come back in a couple of months if this persists".
 '''
+print("\nThank you for using Eliza300, a fun therapy program.\nPlease state your emotional complaint then hit 'ENTER':")
+
+user_input = input()
+
+print('How many months has it been that you have experienced "' + user_input + '"')
+
+months = input()
+if 1 <= int(months) >=100: 
+    print("Please try running the system again and enter months between 1 and 100 in months less than 100.")
+    import sys
+    sys.exit()
+if int(months) <= 2: 
+    print ("Come back in a couple of months if this persists")
+if int(months) >=3:
+    print(months + " months is too much time to go without help! Lets schedule a few sessions.")
